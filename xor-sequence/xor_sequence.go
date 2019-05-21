02ecd50deac3d1bee61bb0a7e87f80f4e9a20a1b
Wojciech Gac <wojciech.s.gac@gmail.com>
2019-05-21 16:13:24 +0200

    Fix XOR sequence and make it perform

--- xor-sequence/xor_sequence.go
+++ xor-sequence/xor_sequence.go
@@ -1,13 +1,19 @@
 package xor_sequence
 
-func xorSequence(l int64, r int64) int64 {
-	result := int64(0)
-	a := int64(0)
-	for i := int64(0); i <= r; i++ {
-		a ^= i
-		if i >= l {
-			result ^= a
-		}
+func process(x int64) int64 {
+	a := x % 8
+	switch {
+	case a == 0 || a == 1:
+		return x
+	case a == 2 || a == 3:
+		return 2
+	case a == 4 || a == 5:
+		return x + 2
+	default:
+		return 0
 	}
-	return result
+}
+
+func xorSequence(l int64, r int64) int64 {
+	return process(r) ^ process(l-1)
 }
